3d8acee1d866b7d7228f2f7eeabe3ce2aa8ca667
v1nvn <v1n@outlook.com>
2025-11-08 17:01:08 +0530

    fix: remove devtools on build

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
+import { devtools } from '@tanstack/devtools-vite'
 
 import { d1InitPlugin } from './vite-plugins/d1-init'
 
 const config = defineConfig({
   plugins: [
+    devtools({
+      removeDevtoolsOnBuild: true,
+    }),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     d1InitPlugin(), // Auto-run D1 migrations and seeding in dev mode
     // this is the plugin that enables path aliases
